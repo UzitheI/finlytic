b18c10801bab1148f2f63e2bf9e99f227f3ddb03
jinesh <jineshbasnet6@gmail.com>
2024-09-28 01:36:03 +0545

    about_section_changes

--- finlytic_web/settings.py
+++ finlytic_web/settings.py
@@ -82,14 +82,15 @@ WSGI_APPLICATION = 'finlytic_web.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT')
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'defaultdb',
+        'USER': 'avnadmin',
+        'PASSWORD': 'AVNS_hnsFamn50TCdlvF77iD',
+        'HOST': 'finlytic1-ujjwalpuri221-af80.j.aivencloud.com',
+        'PORT': 16161
     }
 }
 
