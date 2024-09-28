5b50b6efac054467d97dc1849f3b295494daa80f
UzitheI <ujjwalpuri221@gmail.com>
2024-09-28 14:52:15 +0545

    date changed

--- dashboard/models.py
+++ dashboard/models.py
@@ -5,3 +5,4 @@ class DataInput(models.Model):
     user=models.ForeignKey(User, on_delete=models.CASCADE)
     excel_file = models.FileField(upload_to='excel_files/')
     query=models.CharField(max_length=250)
+    sent_date=models.DateField(auto_now=True)
