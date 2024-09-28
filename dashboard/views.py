750175fc158035189293ca8a88334646483a9476
UzitheI <ujjwalpuri221@gmail.com>
2024-09-28 14:50:59 +0545

    dashboard data list

--- dashboard/views.py
+++ dashboard/views.py
@@ -27,4 +27,12 @@ class InputList(ListView):
     model= DataInput
     template_name='dashboard/data_list.html'
     
+    def get_context_data(self, **kwargs):
+        personal_user=self.request.user
+        context = super().get_context_data(**kwargs)
+        context["items"] = DataInput.objects.filter(user=personal_user) 
+        return context
+    
+    
+    
     
